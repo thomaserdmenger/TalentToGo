ca0e19fb4977b69602ba2e2324b18234f9043409
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-05-09 18:05:40 +0200

    Add Placement Process Section on Employers Page

--- frontend/src/data/employerData.js
+++ frontend/src/data/employerData.js
@@ -7,3 +7,89 @@ export const employerDataUS = {
   heading: "TalentToGo: We find the specialists you urgently need",
   text: "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.",
 }
+
+export const employerPlacementDE = {
+  features: [
+    {
+      name: "Kontakt",
+      description:
+        "Sie teilen uns Ihre Wünsche und Anforderungen für Ihre Kandidaten mit einem Suchauftrag mit.",
+    },
+    {
+      name: "Matching",
+      description:
+        "Wir präsentieren Ihnen passende Kandidaten aus unserem Pool. Sie treffen die Wunschkandidaten per Videokonferenz und schließen dann einen Vermittlungsvertrag mit TalentToGo und einen Arbeitsvertrag mit Ihrer Fachkraft ab.",
+    },
+    {
+      name: "Deutschkurs",
+      description:
+        "Für ein Visum benötigen Ihre zukünftigen Mitarbeiter Deutschkenntnisse, die von einer zertifizierten Institution in Mexiko bescheinigt werden. Dieser Prozess dauert etwa 6 Monate und entspricht dem Niveau A2.",
+    },
+    {
+      name: "Visumantrag",
+      description:
+        "Wir bereiten alle benötigten Visumdokumente vor und übernehmen für Sie die bürokratischen Angelegenheiten.",
+    },
+    {
+      name: "Ankunft",
+      description:
+        "Mit dem Visum im Gepäck kann die Fachkraft bereits als Pflegehilfskraft bei Ihnen mit 20 Stunden pro Woche arbeiten. Der Arbeitgeber stellt eine Unterkunft zur Verfügung, bis die Anerkennung erreicht ist. Unser Relocationservice begleitet Sie bei weiteren Behördengängen.",
+    },
+    {
+      name: "Anerkennung",
+      description:
+        "Nachdem der Antrag auf Anerkennung der ausländischen Qualifikation gestellt wurde, wird weiterhin Deutsch gelernt, bis das B2-Niveau erreicht ist. Anschließend wird ein Anpassungslehrgang besucht und die Kenntnisprüfung abgelegt.",
+    },
+    {
+      name: "Ihre Fachkraft",
+      description:
+        "Nach bestandener Anerkennung bleibt die Fachkraft gemäß Vereinbarung im Arbeitsvertrag für zwei Jahre im Unternehmen, um Sie zu unterstützen.",
+    },
+  ],
+  slogan: "Slogan oder Call to Action",
+  heading: "Prozess der Fachkräftevermittlung",
+  text: " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+}
+
+export const employerPlacementUS = {
+  features: [
+    {
+      name: "Contact",
+      description:
+        "You tell us your wishes and requirements for your candidates with a search request.",
+    },
+    {
+      name: "Matching",
+      description:
+        "We present you with suitable candidates from our pool. You meet the desired candidates via video conference and then conclude a placement contract with TalentToGo and an employment contract with your specialist.",
+    },
+    {
+      name: "German course",
+      description:
+        "In order to obtain a visa, your future employees need German language skills that are certified by a certified institution in Mexico. This process takes about 6 months and corresponds to level A2.",
+    },
+    {
+      name: "Visa application",
+      description:
+        "We prepare all the necessary visa documents and take care of the bureaucratic matters for you.",
+    },
+    {
+      name: "Arrival",
+      description:
+        "With the visa in hand, the specialist can already work for you as a care assistant for 20 hours a week. The employer will provide accommodation until recognition has been obtained. Our relocation service will assist you with further administrative procedures.",
+    },
+    {
+      name: "Recognition",
+      description:
+        "After the application for recognition of the foreign qualification has been submitted, German continues to be studied until the B2 level is reached. An adaptation course is then attended and the knowledge test is taken.",
+    },
+    {
+      name: "Your specialist",
+      description:
+        "Once they have been recognized, the specialist will remain with the company for two years to support you, as agreed in the employment contract.",
+    },
+  ],
+  slogan: "Slogan or Call to Action",
+  heading: "The process of recruiting specialists",
+  text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+}
--- frontend/src/pages/EmployerPage.jsx
+++ frontend/src/pages/EmployerPage.jsx
@@ -3,8 +3,14 @@ import Hero from "../components/Hero"
 import { employerDataDE, employerDataUS } from "../data/employerData.js"
 import { homeDataHeroDE, homeDataHeroUS } from "../data/homeData.js"
 import { navigationDE, navigationUS } from "../data/navigation.js"
+import { employerPlacementDE, employerPlacementUS } from "../data/employerData.js"
+import { CheckIcon } from "@heroicons/react/20/solid"
+import { useContext } from "react"
+import { languageContext } from "../Context/Context.jsx"
 
 const EmployerPage = () => {
+  const { language } = useContext(languageContext)
+
   return (
     <>
       <header>
@@ -17,7 +23,76 @@ const EmployerPage = () => {
           employerDataUS={employerDataUS}
         />
       </header>
-      <main></main>
+      <main>
+        {/* Placement Process Section */}
+        <section className="placementProcessSection">
+          <div className="bg-white py-24 sm:py-32">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+              <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:max-w-none lg:grid-cols-2 ">
+                {language === "DE" && (
+                  <>
+                    <div className="lg:text-center col-span-2 ">
+                      <h2 className="text-base font-semibold leading-7 text-indigo-600">
+                        {employerPlacementDE.slogan}
+                      </h2>
+                      <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                        {employerPlacementDE.heading}
+                      </p>
+                      <p className="mt-6 text-base leading-7 text-gray-600">
+                        {employerPlacementDE.text}
+                      </p>
+                    </div>
+                    <dl className="col-span-2 grid grid-cols-1 gap-x-8 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:gap-y-16">
+                      {employerPlacementDE.features.map((feature) => (
+                        <div key={feature.name} className="relative pl-9">
+                          <dt className="font-semibold text-gray-900">
+                            <CheckIcon
+                              className="absolute left-0 top-1 h-5 w-5 text-indigo-500"
+                              aria-hidden="true"
+                            />
+                            {feature.name}
+                          </dt>
+                          <dd className="mt-2">{feature.description}</dd>
+                        </div>
+                      ))}
+                    </dl>
+                  </>
+                )}
+
+                {language === "US" && (
+                  <>
+                    <div className="lg:text-center col-span-2 ">
+                      <h2 className="text-base font-semibold leading-7 text-indigo-600">
+                        {employerPlacementUS.slogan}
+                      </h2>
+                      <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                        {employerPlacementUS.heading}
+                      </p>
+                      <p className="mt-6 text-base leading-7 text-gray-600">
+                        {employerPlacementUS.text}
+                      </p>
+                    </div>
+                    <dl className="col-span-2 grid grid-cols-1 gap-x-8 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:gap-y-16">
+                      {employerPlacementUS.features.map((feature) => (
+                        <div key={feature.name} className="relative pl-9">
+                          <dt className="font-semibold text-gray-900">
+                            <CheckIcon
+                              className="absolute left-0 top-1 h-5 w-5 text-indigo-500"
+                              aria-hidden="true"
+                            />
+                            {feature.name}
+                          </dt>
+                          <dd className="mt-2">{feature.description}</dd>
+                        </div>
+                      ))}
+                    </dl>
+                  </>
+                )}
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
       <Footer />
     </>
   )
