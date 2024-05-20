6846d787809ceaba38b1c0db0504f5f6fab5afaf
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-05-20 15:20:12 +0200

    Add Images to Hero Section

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,38 @@ export default {
       fontFamily: {
         sans: ["Inter", "sytem-ui"],
       },
+      backgroundImage: {
+        "hero-home": "url('/images/Hero_Home.jpg')",
+        "hero-employers": "url('/images/Hero_Employer.jpg')",
+        "hero-employees": "url('/images/Hero_Employees.jpg')",
+      },
+      textShadow: {
+        sm: "1px 1px 2px rgba(0, 0, 0, 0.5)",
+        DEFAULT: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+        lg: "3px 3px 6px rgba(0, 0, 0, 0.5)",
+      },
     },
   },
-  plugins: [require("@tailwindcss/aspect-ratio"), require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/forms"),
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".text-shadow-sm": {
+          textShadow: "1px 1px 2px rgba(0, 0, 0, 0.5)",
+        },
+        ".text-shadow": {
+          textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+        },
+        ".text-shadow-lg": {
+          textShadow: "3px 3px 6px rgba(0, 0, 0, 0.5)",
+        },
+        ".text-shadow-none": {
+          textShadow: "none",
+        },
+      }
+
+      addUtilities(newUtilities, ["responsive", "hover"])
+    },
+  ],
 }
