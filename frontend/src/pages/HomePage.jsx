b6073c3c64102ed6016e3eddf5c177ef0724056f
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-05-26 19:54:03 +0200

    Update Profile Pictures in About Us Section

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -230,16 +230,16 @@ const HomePage = () => {
                   </div>
                   <ul
                     role="list"
-                    className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+                    className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
                     {aboutUsDE.people.map((person, index) => (
                       <li key={index}>
                         <img
-                          className="aspect-[3/2] w-full rounded-2xl object-cover"
+                          className="aspect-[1] w-full rounded-2xl object-cover object-top"
                           src={person.imageUrl}
                           alt=""
                         />
                         <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">
-                          {person.name}
+                          {person.fullname}
                         </h3>
                         <p className="text-base leading-7 text-gray-600">{person.role}</p>
                         <ul role="list" className="mt-6 flex gap-x-6">
@@ -292,16 +292,16 @@ const HomePage = () => {
                   </div>
                   <ul
                     role="list"
-                    className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+                    className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
                     {aboutUsES.people.map((person, index) => (
                       <li key={index}>
                         <img
-                          className="aspect-[3/2] w-full rounded-2xl object-cover"
+                          className="aspect-[1] w-full rounded-2xl object-cover object-top"
                           src={person.imageUrl}
                           alt=""
                         />
                         <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">
-                          {person.name}
+                          {person.fullname}
                         </h3>
                         <p className="text-base leading-7 text-gray-600">{person.role}</p>
                         <ul role="list" className="mt-6 flex gap-x-6">
