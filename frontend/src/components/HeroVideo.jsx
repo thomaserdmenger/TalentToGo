2198ccd0664fd21b97ecb262912bfb587e1db575
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-06-02 11:26:49 +0200

    Delete Social Media Icons in Nav Bar

--- frontend/src/components/HeroVideo.jsx
+++ frontend/src/components/HeroVideo.jsx
@@ -120,6 +120,7 @@ export default function HeroVideo({
       <header className="absolute inset-x-0 top-0 z-50">
         {/* Desktop Menu */}
         <nav className="flex items-center justify-between p-6 lg:px-8 relative" aria-label="Global">
+          {/* Logo Container */}
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Talent To Go</span>
@@ -130,6 +131,7 @@ export default function HeroVideo({
               />
             </a>
           </div>
+          {/* Menü Bar Icon */}
           <div className="flex lg:hidden">
             <button
               type="button"
@@ -139,6 +141,7 @@ export default function HeroVideo({
               <Bars3Icon className="h-6 w-6" aria-hidden="true" />
             </button>
           </div>
+          {/* Nav Links */}
           <div className="hidden lg:flex lg:gap-x-12">
             {language === "DE" &&
               navigationDE.map((item) =>
@@ -254,10 +257,8 @@ export default function HeroVideo({
                 )
               )}
           </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end lg:gap-2">
-            {/* <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-              Log in <span aria-hidden="true">&rarr;</span>
-            </a> */}
+          {/* Social Media Section */}
+          {/* <div className="hidden lg:flex lg:flex-1 lg:justify-end lg:gap-2">
             <a href="https://www.facebook.com/" target="_blank">
               <FaSquareFacebook size={20} />
             </a>
@@ -267,7 +268,8 @@ export default function HeroVideo({
             <a href="#">
               <FaSquareWhatsapp size={20} />
             </a>
-          </div>
+          </div> */}
+          {/* Language Menü */}
           <div className="fixed top-20 right-6 md:right-8">
             <LanguageSelect />
           </div>
@@ -351,19 +353,6 @@ export default function HeroVideo({
           muted
           loop></video>
 
-        {/* Hintergrundeffekt oben links */}
-        {/* <div
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          aria-hidden="true">
-          <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-          />
-        </div> */}
-
         {language === "DE" && (
           <div className="mx-auto max-w-4xl relative">
             <div className="text-center px-6">
@@ -377,11 +366,6 @@ export default function HeroVideo({
                 {pathname === "/employer" && employerDataDE.subheading}
                 {pathname === "/employees" && employeesDataDE.subheading}
               </h2>
-              {/* <p className="mt-6 text-lg leading-8 text-white max-w-2xl mx-auto">
-                {pathname === "/" && homeDataHeroDE.text}
-                {pathname === "/employer" && employerDataDE.text}
-                {pathname === "/employees" && employeesDataDE.text}
-              </p> */}
               {pathname === "/" && (
                 <div className="mt-10 flex items-center justify-center gap-x-6">
                   <Link
@@ -413,11 +397,6 @@ export default function HeroVideo({
                 {pathname === "/employer" && employerDataES.subheading}
                 {pathname === "/employees" && employeesDataES.subheading}
               </h2>
-              {/* <p className="mt-6 text-lg leading-8 text-white">
-                {pathname === "/" && homeDataHeroES.text}
-                {pathname === "/employer" && employerDataES.text}
-                {pathname === "/employees" && employeesDataES.text}
-              </p> */}
               {pathname === "/" && (
                 <div className="mt-10 flex items-center justify-center gap-x-6">
                   <Link
@@ -435,19 +414,6 @@ export default function HeroVideo({
             </div>
           </div>
         )}
-
-        {/* Hintergrundeffekt unten rechts */}
-        {/* <div
-          className="question absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-          aria-hidden="true">
-          <div
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            }}
-          />
-        </div> */}
       </div>
     </div>
   );
